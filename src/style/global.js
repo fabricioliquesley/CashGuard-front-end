10472547e4cfc84b17c125d0bbfc7fdb995cec7d
Fabricio <fabricioliquesley15@gmail.com>
2024-01-24 09:28:43 -0300

    feat: defined the overall style for the project

--- src/style/global.js
+++ src/style/global.js
@@ -1,7 +1,36 @@
 import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    :root {
+        font-size: 12px;
+    }
+
     body {
-        background: ${({theme}) => theme.COLORS.GRAY_500};
+        background: ${({ theme }) => theme.COLORS.GRAY_500};
+    }
+
+    body, input, button {
+        font-family: 'Roboto', sans-serif;
+        font-size: 1rem;
+        outline: none;
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    button, a {
+        cursor: pointer;
+        transition: filter 0.2s;
+    }
+
+    button:hover, a:hover {
+        filter: brightness(0.9);
     }
 `;
