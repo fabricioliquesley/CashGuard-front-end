4985532bacb796d5f5c4d851a0d6c6ea4e169c8d
Fabricio <fabricioliquesley15@gmail.com>
2024-01-23 22:35:29 -0300

    init the project

---
+++ src/style/theme.js
@@ -0,0 +1,9 @@
+export default {
+    COLORS: {
+        BLUE_100: "#889ABF",
+        GRAY_200: "#737272",
+        GRAY_300: "#404040",
+        GRAY_400: "#202126",
+        GRAY_500: "#0A0B0D",
+    }
+}
