1ba2cc9da1c3f76e31833d3b3d25aae5b6a47af9
Fabricio <fabricioliquesley15@gmail.com>
2024-01-24 23:27:56 -0300

    feat: finalized the transaction page

--- src/pages/Transactions/index.jsx
+++ src/pages/Transactions/index.jsx
@@ -1,10 +1,72 @@
-import { Container } from "./style";
+import { Container, Content, Filter } from "./style";
 import { Menu } from "../../components/Menu";
+import { Transaction } from "../../components/Transaction";
+import { ButtonFilter } from "../../components/ButtonFilter";
+import { FaFilter } from "react-icons/fa";
+import { useState } from "react";
 
 export function Transactions() {
+    const [buttonSelected, setButtonSelected] = useState("");
+
+    function handleSelectedButton(button){
+        setButtonSelected(button);
+    }
+
     return (
         <Container>
-            <h3>Transações</h3>
+            <Content>
+                <h2>Transações</h2>
+                <Filter>
+                    <FaFilter />
+                    <ButtonFilter
+                        name={"todas"} 
+                        isActive={buttonSelected === "" || buttonSelected === "all"}
+                        onClick={() => handleSelectedButton("all")}
+                    />
+                    <ButtonFilter 
+                        name={"receitas"}
+                        isActive={buttonSelected === "incomes"}
+                        onClick={() => handleSelectedButton("incomes")}
+                    />
+                    <ButtonFilter 
+                        name={"despesas"}
+                        isActive={buttonSelected === "expenses"}
+                        onClick={() => handleSelectedButton("expenses")}
+                    />
+                </Filter>
+                <section>
+                    <Transaction
+                        type={"expenses"}
+                        title={"Água"}
+                        value={20.56}
+                        date={"2024-01-24"}
+                    />
+                    <Transaction
+                        type={"incomes"}
+                        title={"Água"}
+                        value={20.56}
+                        date={"2024-01-24"}
+                    />
+                    <Transaction
+                        type={"expenses"}
+                        title={"Água"}
+                        value={20.56}
+                        date={"2024-01-24"}
+                    />
+                    <Transaction
+                        type={"incomes"}
+                        title={"Água"}
+                        value={20.56}
+                        date={"2024-01-24"}
+                    />
+                    <Transaction
+                        type={"expenses"}
+                        title={"Água"}
+                        value={20.56}
+                        date={"2024-01-24"}
+                    />
+                </section>
+            </Content>
             <Menu />
         </Container>
     )
--- src/pages/Transactions/style.js
+++ src/pages/Transactions/style.js
@@ -6,3 +6,28 @@ export const Container = styled.main`
     width: 100%;
     min-height: 100vh;
 `;
+
+export const Content = styled.section`
+    padding: 1.66666rem;
+
+    > h2 {
+        color: ${({theme}) => theme.COLORS.GRAY_200};
+    }
+
+    > section {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+    }
+`;
+
+export const Filter = styled.nav`
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    margin: 1rem 0 2rem;
+    
+    svg {
+        color: ${({theme}) => theme.COLORS.BLUE_100};
+    }
+`;
