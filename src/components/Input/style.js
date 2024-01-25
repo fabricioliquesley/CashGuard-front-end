a45d5da3f45edf778bc5ba2b4fb2d9f20ff94e2e
Fabricio <fabricioliquesley15@gmail.com>
2024-01-25 17:41:33 -0300

    feat: created the input component

---
+++ src/components/Input/style.js
@@ -0,0 +1,27 @@
+import { styled } from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    width: 100%;
+    padding: 0.7rem;
+    border: 1px solid ${({theme}) => theme.COLORS.BLUE_100};
+    border-radius: 1rem;
+    color: ${({theme}) => theme.COLORS.BLUE_100};
+
+    > svg {
+        font-size: 1.2rem;
+    }
+
+    > input {
+        width: 100%;
+        background-color: transparent;
+        color: ${({theme}) => theme.COLORS.BLUE_100};
+        border: none;
+
+        &::placeholder {
+            color: ${({theme}) => theme.COLORS.BLUE_100};
+        }
+    }
+`;
