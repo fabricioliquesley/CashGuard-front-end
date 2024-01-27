d957b9e917a31d36e431d315e6f373fc7999408e
Fabricio <fabricioliquesley15@gmail.com>
2024-01-26 21:47:32 -0300

    feat: created the responsiveness of the transaction screen

--- src/pages/Transactions/style.js
+++ src/pages/Transactions/style.js
@@ -1,10 +1,17 @@
 import { styled } from "styled-components";
+import { DEVICE_BREAKPOINT } from "../../style/deviceBreakPoint";
 
 export const Container = styled.main`
     display: grid;
     grid-template-rows: 1fr 60px;
     width: 100%;
     min-height: 100vh;
+
+    @media (min-width: ${DEVICE_BREAKPOINT.LG}) {
+        display: flex;
+        flex-direction: row-reverse;
+        overflow: hidden;
+    }
 `;
 
 export const Content = styled.section`
@@ -16,15 +23,26 @@ export const Content = styled.section`
         justify-content: space-between;
         
         h2 {
-            color: ${({theme}) => theme.COLORS.GRAY_200};
+            color: ${({ theme }) => theme.COLORS.GRAY_200};
         }
     }
-
 
     > section {
         display: flex;
         flex-direction: column;
         gap: 1rem;
+    }
+
+    @media (min-width: ${DEVICE_BREAKPOINT.LG}) {
+        flex: 1;
+        height: 100vh;
+        overflow-y: auto;
+
+        > section {
+            width: 100%;
+            max-width: 51.875rem;
+            margin: 0 auto;
+        }
     }
 `;
 
@@ -35,6 +53,6 @@ export const Filter = styled.nav`
     margin: 1rem 0 2rem;
     
     svg {
-        color: ${({theme}) => theme.COLORS.BLUE_100};
+        color: ${({ theme }) => theme.COLORS.BLUE_100};
     }
 `;
